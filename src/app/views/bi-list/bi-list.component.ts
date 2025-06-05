a332b4fd977cd62b3d7b881e9e2c67db2e98d87d
PAULO EDUARDO FURTADO LOPES <pelopes@fgr.com.br>
2025-06-05 17:00:53 -0300

    feat/ nova abordagem

--- src/app/views/bi-list/bi-list.component.ts
+++ src/app/views/bi-list/bi-list.component.ts
@@ -1,26 +1,38 @@
-import {Component, computed, inject, signal} from '@angular/core';
+import {Component, computed, inject, OnDestroy, signal, ViewChild} from '@angular/core';
 import {BiCardComponent} from '@components/bi-card/bi-card.component';
 import {BiListService} from '@services/bi-list-service/bi-list.service';
 import {BiItem, Department, Tag} from '../../core/models/posts/posts.model';
-import {Observable} from 'rxjs';
 import {FormsModule} from '@angular/forms';
 import {MatFormFieldModule} from '@angular/material/form-field';
 import {MatInputModule} from '@angular/material/input';
 import {MatSelectModule} from '@angular/material/select';
 import {MatChipsModule} from '@angular/material/chips';
 import {MatButtonModule} from '@angular/material/button';
+import {MatListItem, MatListModule, MatNavList} from '@angular/material/list';
+import {MatSidenav, MatSidenavContainer, MatSidenavContent, MatSidenavModule} from '@angular/material/sidenav';
+import {MatToolbar, MatToolbarModule} from '@angular/material/toolbar';
+import {CommonModule, NgOptimizedImage} from '@angular/common';
+import {MediaMatcher} from '@angular/cdk/layout';
+import {MatIcon, MatIconModule} from '@angular/material/icon';
 
 @Component({
   selector: 'app-bi-list',
-  imports: [BiCardComponent, FormsModule, MatFormFieldModule, MatInputModule, MatSelectModule, MatChipsModule, MatButtonModule],
+  imports: [CommonModule, MatIconModule, MatToolbarModule, MatButtonModule, MatIconModule, MatSidenavModule, MatListModule, BiCardComponent, FormsModule, MatFormFieldModule, MatInputModule, MatSelectModule, MatChipsModule, MatButtonModule, MatIcon, MatListItem, MatNavList, MatSidenav, MatSidenavContainer, MatSidenavContent, MatToolbar, NgOptimizedImage],
   templateUrl: './bi-list.component.html',
   styleUrl: './bi-list.component.scss'
 })
-export class BiListComponent {
+export class BiListComponent implements OnDestroy {
+
+  @ViewChild(MatSidenav)
+  sidenav!: MatSidenav;
+  readonly isCollapsed = signal(true);
+
+  protected readonly isMobile = signal(true);
+  private readonly _mobileQuery: MediaQueryList;
+  private readonly _mobileQueryListener: () => void;
+
 
   private biListService = inject(BiListService);
-
-  protected biList$: Observable<BiItem[]> = this.biListService.getPosts();
 
   // Signals puros
   protected biItems = signal<BiItem[]>([]);
@@ -56,6 +68,23 @@ export class BiListComponent {
 
   constructor() {
     this.loadData();
+    const media = inject(MediaMatcher);
+
+    this._mobileQuery = media.matchMedia('(max-width: 600px)');
+    this.isMobile.set(this._mobileQuery.matches);
+    this._mobileQueryListener = () => this.isMobile.set(this._mobileQuery.matches);
+    this._mobileQuery.addEventListener('change', this._mobileQueryListener);
+
+  }
+
+  toggleMenu() {
+    if (this.isMobile()) {
+      this.sidenav.toggle();
+      this.isCollapsed.set(false);
+    } else {
+      this.sidenav.open();
+      this.isCollapsed.set(!this.isCollapsed());
+    }
   }
 
   private loadData(): void {
@@ -106,8 +135,9 @@ export class BiListComponent {
     return this.selectedTags().includes(tagId);
   }
 
-
-
+  ngOnDestroy(): void {
+    this._mobileQuery.removeEventListener('change', this._mobileQueryListener);
+  }
 
 
 }
